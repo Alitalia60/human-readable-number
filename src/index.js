468e362a822644c5ff45ca5e07fc2e3211cbf37c
Andrew Litvinov <lita.study@bk.ru>
2022-02-06 23:44:10 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const i_01_09 = [
-    "zero",
+    "",
     "one",
     "two",
     "three",
@@ -11,7 +11,7 @@ const i_01_09 = [
     "nine",
 ];
 const i_11_19 = [
-    "",
+    "ten",
     "eleven",
     "twelve",
     "thirteen",
@@ -35,83 +35,54 @@ const dozens = [
     "eighty",
     "ninety",
 ];
-const levels = ["", "hundred", "thousand", "million", "billion"];
-let res = "";
+const levels = ["", "", "thousand", "million", "billion"];
 
-function numberToWord(number) {
-    let level = 0;
-    do {
-        let lastDigit = number - Math.floor(number / 10) * 10;
-        number = Math.floor(number / 10);
-        res = sayDigits(lastDigit) + levels[level] + " " + res;
-        //if level =0,  digits
-        level = level == 2 ? 0 : level;
-    } while (number >= 1);
-    return res;
-}
+module.exports = function toReadable(n) {
+    let res = "";
+    let strN = String(n);
 
-let n = 112;
-let strN = String(n);
-
-if (n === 0) {
-    console.log('zero');
-    return 'zero'
-}
-
-console.log("входное число =", strN);
-
-let triadaCount = Math.ceil(strN.length / 3);
-let arrayOfTriple = [];
-while (triadaCount > 0) {
-    let triada = strN.slice(strN.length - 3);
-    switch (triada.length) {
-        case 1:
-            triada = "00" + triada;
-            break;
-        case 2:
-            triada = "0" + triada;
-            break;
-        default:
-            break;
+    if (n === 0) {
+        return 'zero'
     }
-    arrayOfTriple.push(triada);
-    strN = strN.slice(0, strN.length - 3);
-    triadaCount--;
+
+    let triadaCount = Math.ceil(strN.length / 3);
+    let arrayOfTriple = [];
+    while (triadaCount > 0) {
+        let triada = strN.slice(Math.max(0, strN.length - 3));
+
+        triada = triada.padStart(3, "0");
+
+        arrayOfTriple.push(triada);
+        strN = strN.slice(0, Math.max(0, strN.length - 3));
+        triadaCount--;
+    }
+
+    for (let level = arrayOfTriple.length - 1; level >= 0; level--) {
+        res += parseTriada(arrayOfTriple[level]) + levels[level + 1] + " "
+    }
+    return res.trim()
 }
 
-for (let level = 1; level <= arrayOfTriple.length; level++) {
-    res = "";
-    const triada = arrayOfTriple[level - 1];
-    console.log("level=", level, 'разбор=', triada);
 
-    if (Number(triada) >= 100) {
-        res += i_01_09[Number(triada[0])] + " " + levels[level] + " ";
-        //проверка числа сотен
-        // console.log(i_01_09[Number(triada[0])], levels[level]);
+function parseTriada(triada) {
+    resTriada = "";
+    let numTriada = Number(triada);
+    if (numTriada >= 100) {
+        resTriada += i_01_09[Number(triada[0])] + " hundred ";
 
-        //проверка числа десятков
+        numTriada = numTriada - Number(triada[0]) * 100
     }
-    if (Number(triada.slice(1)) >= 20) {
-        res += dozens[Number(triada[1])] + "-";
-    } else if (Number(triada.slice(1)) >= 10) {
-        res += i_11_19[Number(triada.slice(1)) - 10] + " ";
+    if (numTriada >= 20) {
+        resTriada += dozens[Number(triada[1])];
+        if (Number(triada[2]) > 0) {
+            resTriada += " " + i_01_09[Number(triada[2])]
+        }
+    } else if (numTriada >= 10) {
+        resTriada += i_11_19[numTriada - 10] + " ";
     } else {
-        res += i_01_09[Number(triada[2])] + " ";
+        resTriada += i_01_09[Number(triada[2])] + " ";
     }
-
-    console.log(res);
+    return resTriada
 }
 
-
-// numberToWord(123456)
-// console.log(numberToWord(618604283947))
-// console.log(from1to19(1));
-// console.log(from1to19(5));
-// console.log(from1to19(8));
-// console.log(from1to19(9));
-// console.log(from1to19(10));
-// console.log(from1to19(12));
-// console.log(from1to19(13));
-// console.log(from1to19(16));
-// console.log(from1to19(19));
-// console.log(from1to19(20));
+// console.log(toReadable(323));
